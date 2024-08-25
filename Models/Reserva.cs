ea23adb6b8e508814d67215b11292d8cda3fd464
Leo Tachini <leo1tachini@gmail.com>
2024-08-25 18:08:38 -0300

    feat: adicionando calculo de reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,46 +15,44 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count <= Suite.Capacidade)
             {
+                Console.WriteLine("Hóspedes cadastrados com sucesso!");
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception("Capacidade máxima de hóspedes excedida.");
             }
         }
 
         public void CadastrarSuite(Suite suite)
-        {
+        {   
             Suite = suite;
         }
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = Suite.ValorDiaria * DiasReservados;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = valor * 0.9m;
+                return valor;
             }
-
-            return valor;
+            else if(DiasReservados <= 0)
+            {
+                throw new Exception("A quantidade de dias reservados deve ser maior que zero.");
+            }
+            else
+            {
+                return valor;
+            }
         }
     }
 }
